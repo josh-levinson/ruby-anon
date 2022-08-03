fb0583036c2720d4fc04ed44d405932dfea189da
Josh Levinson <jlevinson@linksquares.com>
2022-08-03 15:20:33 -0400

    Blacklist strategy requires primary id on every table

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@
 source 'https://rubygems.org'
 
 gem 'data-anonymization'
+gem 'pg'
 gem 'sqlite3'
--- blacklist.rb
+++ blacklist.rb
@@ -1,18 +1,27 @@
-system 'bundle exec ruby whitelist.rb'
+# frozen_string_literal: true
 
 require 'data-anonymization'
+require 'pg'
 
 DataAnon::Utils::Logging.logger.level = Logger::INFO
 
 database 'parvati_development' do
   strategy DataAnon::Strategy::Blacklist
-  source_db adapter: 'postgres', database: 'parvati_development'
+  source_db adapter: 'postgresql',
+            host: ENV['DATABASE_HOST'] || 'db',
+            database: ENV['DATABASE_NAME'] || 'parvati_development',
+            username: ENV['DATABASE_USER'] || 'postgres',
+            password: ENV['DATABASE_PASSWORD'] || 'postgres',
+            port: ENV['DATABASE_PORT'] || 5432,
+            pool: 5,
+            timeout: 5000
 
   table 'agreement_support_urls' do
     anonymize('url_data').using FieldStrategy::RandomString.new
   end
 
   table 'agreements' do
+    primary_key 'id'
     anonymize('name').using FieldStrategy::RandomString.new
     anonymize('description').using FieldStrategy::RandomString.new
     anonymize('instructions').using FieldStrategy::RandomString.new
@@ -28,10 +37,12 @@ database 'parvati_development' do
   end
 
   table 'company_allowlist_emails' do
+    primary_key 'id'
     anonymize('domain').using FieldStrategy::RandomString.new
   end
 
   table 'messages' do
+    primary_key 'id'
     anonymize('sender').using FieldStrategy::RandomFullName.new
     anonymize('subject').using FieldStrategy::RandomString.new
     # TODO: Add to, cc, bcc array fields
@@ -39,31 +50,37 @@ database 'parvati_development' do
   end
 
   table 'recipient_requests' do
+    primary_key 'id'
     anonymize('name').using FieldStrategy::RandomString.new
     anonymize('email').using FieldStrategy::RandomEmail.new
   end
 
   table 'request_forms' do
+    primary_key 'id'
     anonymize('name').using FieldStrategy::RandomString.new
     anonymize('description').using FieldStrategy::RandomString.new
     anonymize('instructions').using FieldStrategy::RandomString.new
   end
 
   table 'request_template_questions' do
+    primary_key 'id'
     anonymize('description').using FieldStrategy::RandomString.new
   end
 
   table 'task_templates' do
+    primary_key 'id'
     anonymize('text').using FieldStrategy::RandomString.new
     anonymize('name').using FieldStrategy::RandomString.new
   end
 
   table 'tasks' do
+    primary_key 'id'
     anonymize('text').using FieldStrategy::RandomString.new
     anonymize('name').using FieldStrategy::RandomString.new
   end
 
   table 'templates' do
+    primary_key 'id'
     anonymize('name').using FieldStrategy::RandomString.new
     anonymize('description').using FieldStrategy::RandomString.new
   end
